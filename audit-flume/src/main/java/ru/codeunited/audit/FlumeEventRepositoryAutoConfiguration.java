103056c76b370e9489030697fa88b260c780511b
ikonovalov <konovalov84@gmail.com>
2019-04-10 00:10:51 +0300

    Reduce unused configuration items

--- audit-flume/src/main/java/ru/codeunited/audit/FlumeEventRepositoryAutoConfiguration.java
+++ audit-flume/src/main/java/ru/codeunited/audit/FlumeEventRepositoryAutoConfiguration.java
@@ -1,11 +1,9 @@
 package ru.codeunited.audit;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.AliasFor;
 
 @Configuration
 @EnableConfigurationProperties(FlumeEventRepositoryProperties.class)
@@ -15,13 +13,6 @@ import org.springframework.core.annotation.AliasFor;
         havingValue = "true"
 )
 public class FlumeEventRepositoryAutoConfiguration {
-
-    private final FlumeEventRepositoryProperties properties;
-
-    @Autowired
-    public FlumeEventRepositoryAutoConfiguration(FlumeEventRepositoryProperties properties) {
-        this.properties = properties;
-    }
 
     @WithFlume @Bean(initMethod = "doStart", destroyMethod = "doStop")
     public FlumeEventRepository flumeEventRepository(FlumeEventRepositoryProperties properties) {
