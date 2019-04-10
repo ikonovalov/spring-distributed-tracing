df6443c32f56b3d3e47f95d7630b3e44c6f44c8f
ikonovalov <konovalov84@gmail.com>
2019-04-11 01:25:00 +0300

    Prepare RestTemplate proxy

--- service-order/src/main/java/ru/codeunited/service/order/ServiceOrder.java
+++ service-order/src/main/java/ru/codeunited/service/order/ServiceOrder.java
@@ -1,19 +1,46 @@
 package ru.codeunited.service.order;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 public class ServiceOrder {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+    @ConditionalOnProperty(prefix = "services", name="proxy")
+    public RestTemplate restTemplate(
+            RestTemplateBuilder restTemplateBuilder,
+            @Value("${service.proxy}") String proxyUrl) {
+
+        return restTemplateBuilder
+                .requestFactory(() -> {
+                    SimpleClientHttpRequestFactory clientHttpReq = new SimpleClientHttpRequestFactory();
+                    URL prx;
+                    try {
+                        prx = new URL(proxyUrl);
+                    } catch (MalformedURLException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                    Proxy proxy = new Proxy(
+                            Proxy.Type.HTTP, new InetSocketAddress(prx.getHost(), prx.getPort())
+                    );
+                    clientHttpReq.setProxy(proxy);
+                    return clientHttpReq;
+                })
+                .build();
     }
 
     public static void main(String[] args) {
